f4c8043c3ca3470979cf86ce6a46fd1693bdff9f
Javier de Paula <javi830810@gmail.com>
2015-05-26 18:00:57 -0400

    Missing File

---
+++ src/ScaleBridge.Core/Publish/MemoryWebhookStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ScaleBridge.Message.Object;
+
+namespace ScaleBridge.Core
+{
+	public class MemoryWebhookStore:IWebhookStore
+	{
+		private List<Webhook> webhooks;
+
+		public MemoryWebhookStore(List<Webhook>webhooks)
+		{
+			this.webhooks = webhooks;
+		}
+
+		public IEnumerable<Webhook> QueryByEventType(string eventType)
+		{
+
+			return this.webhooks.FindAll (x => x.EventType == eventType);
+		}
+	}
+}
+
